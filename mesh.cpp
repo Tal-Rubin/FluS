c642af0f71ba558b53cb242f92f721300fe0f6fe
iceplussss <iceplussss@gmail.com>
2021-11-25 03:19:13 -0500

    NodesList_ID and NodesList_pos

--- mesh.cpp
+++ mesh.cpp
@@ -4,70 +4,93 @@
 #include <vector>
 using std::vector;
 
-
 class Mesh2D
 {
 private:
+    static const int DIM = 2;
+    
     int N_row;
     int N_col;
 
     int Node_row;
     int Node_col;
 
-    int NodeBeginNum;
-    int ElemBeginNum;
+    // vector<int> NodesList_ID;
+    // vector< vector<double> > NodesList_pos;
 
-    vector< vector<int> > NodeList_ID;
-    vector< vector<double> > NodeList_pos;
-
-    void define_NodeList_ID(){
+    vector<int> define_NodesList_ID(){
         
-        int dim1_NodeList_ID = 1;
-        int dim2_NodeList_ID = Node_row * Node_col;
+        int dim1_NodesList_ID = 1;
+        int dim2_NodesList_ID = Node_row * Node_col;
 
-        vector< vector<int> > NodeList_ID(dim1_NodeList_ID, vector<int> (dim2_NodeList_ID, 0));
+        vector<int> NodesList_ID(dim2_NodesList_ID, 0);
 
         for (int i = 0; i < Node_col; i++){
             for (int j = 0; j< Node_row; j++){
-                NodeList_ID[0][i * Node_row + j] = i * Node_row + j;
+                NodesList_ID[i * Node_row + j] = i * Node_row + j;
             }
         }
+        return NodesList_ID;
+    }
+    vector< vector<double> > define_NodesList_pos(){
 
-        for(int j =0; j < dim2_NodeList_ID; j++){
-            std::cout << 0 <<","<< j <<" = " << NodeList_ID[0][j] << std::endl;
+        int dim1_NodesList_pos = DIM;
+        int dim2_NodesList_pos = Node_row * Node_col;
+
+        vector< vector<double> > NodesList_pos(dim1_NodesList_pos, vector<double> (dim2_NodesList_pos, 0.0));
+        for (int i = 0; i < Node_col; i++){
+            for (int j = 0; j< Node_row; j++){
+                NodesList_pos[0][i * Node_row + j] = -1.0 + i / double(N_col); // x
+                NodesList_pos[1][i * Node_row + j] = 1.0 - j / double(N_row) ; // x
+            }
         }
+        return NodesList_pos;
     }
 
 
 
 public:
-    Mesh2D(int n_row, int n_col, int nodeBeginNum, int elemBeginNum)
+    Mesh2D(int n_row, int n_col)
     {
         N_row = n_row;
         N_col = n_col;
 
-        Node_row = 2 * N_row +1;
-        Node_col = 2 * N_col +1;
-
-        NodeBeginNum = nodeBeginNum;
-        ElemBeginNum = elemBeginNum;
-
-        std::cout << "N_row = " << N_row << std::endl;
-        std::cout << "N_col = " << N_col << std::endl;
-
-        std::cout << "Node_row = " << Node_row << std::endl;
-        std::cout << "Node_col = " << Node_col << std::endl;
+        Node_row = 2 * N_row + 1;
+        Node_col = 2 * N_col + 1;
 
     }
-    vector<vector<int> > get_NodeList_ID(){
-        define_NodeList_ID();
-        return NodeList_ID;
+
+    vector<int>  get_NodesList_ID(){
+
+        vector<int> NodesList_ID = define_NodesList_ID();
+
+        return NodesList_ID;
+
+    }
+    vector< vector<double> >  get_NodesList_pos(){
+
+        vector< vector<double> > NodesList_pos = define_NodesList_pos();
+
+        return NodesList_pos;
+
     }
 };
 
 int main()
 {
-    Mesh2D mesh(1,2,3,4);
-    mesh.get_NodeList_ID();
+    Mesh2D* mesh2d = new Mesh2D(1,2);
 
+    vector<int> nodesList_ID = mesh2d->get_NodesList_ID();
+    vector< vector<double> > nodesList_pos = mesh2d->get_NodesList_pos();
+
+    std::cout << "NodesList_ID size = " << nodesList_ID.size() << std::endl;
+
+    for(int i = 0; i < nodesList_ID.size(); i++){
+        std::cout << i << " = " << nodesList_ID[i] << std::endl;
+    }
+
+    for(int i = 0; i < nodesList_pos.size(); i++){
+        for (int j = 0; j < nodesList_pos[i].size(); j++)
+            std::cout << i << "," << j<< " = " << nodesList_pos[i][j] << std::endl;
+    }
 }
