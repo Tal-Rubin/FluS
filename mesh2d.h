b61027215e8dff2e6e5ff3d1228e607d43d0ba64
iceplussss <iceplussss@gmail.com>
2021-11-25 21:12:32 -0500

    EdgesList added

--- mesh2d.h
+++ mesh2d.h
@@ -10,28 +10,36 @@ using std::vector;
 class Mesh2D : public Mesh {
 
 public:
-    Mesh2D(int n_row, int n_col); // Constructor
-    // ~Mesh2D();                 // Destructor
+    Mesh2D(int n_row, int n_col);  // Constructor
+    // ~Mesh2D();                  // Destructor
 
-    vector<int>  get_NodesList_ID();
-    vector< vector<double> >  get_NodesList_pos();
+    vector<int> get_EdgesList_ID();                // Get the Edges ID
+    vector<int> get_NodesList_ID();                // Get the Nodes ID
+    vector< vector<double> > get_NodesList_pos();  // Get the global Nodes position
 
 private:
 
-    static const int DIM = 2;
+    static const int DIM = 2; // Space dimension
 
-    int Elem_row_;
-    int Elem_col_;
+    int Elem_row_;  // The number of rows and columns for elements
+    int Elem_col_;  
 
-    int Node_row_;
-    int Node_col_;
+    int Node_row_;  // The number of rows and columns for nodes
+    int Node_col_;  
 
-    vector<int> NodesList_ID;
-    vector< vector<double> > NodesList_pos;
+    int num_Nodes;  // The number of Nodes
+    int num_Edges;  // The number of Edges
 
+    vector<int> EdgesList_ID;               // ID of Edges, dim=(num_Edges). 
+    vector<int> NodesList_ID;               // ID of Nodes, dim=(num_Nodes). 
+    vector< vector<double> > NodesList_pos; // x and y coordinates of Nodes, dim=(2, num_Nodes). 
+
+    void define_EdgesList_ID();
     void define_NodesList_ID();
     void define_NodesList_pos();
 
+    void PBE_NodesList_pos();
+    
 };
 
 #endif  // MESH2D_H_
