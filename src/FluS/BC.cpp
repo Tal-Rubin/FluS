c8d39980b72a601b15e853b731760d5e8956f9d9
dingyunl <dingyunl@princeton.edu>
2021-12-14 08:46:25 -0500

    Added BC.cpp

--- src/FluS/BC.cpp
+++ src/FluS/BC.cpp
@@ -7,15 +7,17 @@
 #include <array>
 
 #include "mesh1d.h"
+#include "mesh2d.h"
 
-int BC(int num_Elems, Dynamic_Variable& state, const double t,
-       std::valarray<double> * f_pr(double)) {
+int BC(int num_Elems, std:vector<Elem> elem_vect, Dynamic_Variable& state,
+       const double t, std::valarray<double> * f_pr(double)) {
   /*
    * @brief Member function of a Stepper to set the boundary conditions: 
    *        for the ghost cells, override with a prescribed field and 
    *        calculate the flux through the edge 
    *
    * @param num_Elems Number of real elements
+   * @param elem_vect The vector of elements 
    * @param state A set of Dynamic_Variable
    * @param t Time of the current loop
    * @param f_pr Pointer to the time-independent fields imposed on the ghost cells
@@ -23,19 +25,10 @@ int BC(int num_Elems, Dynamic_Variable& state, const double t,
 
   for (int i = 0; i < num_Elems + 2; i++) {
     // Scan the elements, impose a field if they are ghost elements
-    if (i == 0) {
-      // Ghost cell on the left end
-      state.element(i) = f_pr(t)[0];
+    Elem elem = elem_vect[i];
+    if (elem.ghost == true) {
+      state.element(elem.elem_number) = f_pr[elem.elem_number];
     }
-
-    if (i == num_Elems_ + 1) {
-      // Ghost cell on the right end
-      state.element(i) = f_pr(t)[1];   
-    }
-  }
 
   return 0;
 }
-  
-    
-    
